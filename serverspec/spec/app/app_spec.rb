77d4552ae882523c93657378a1c098c48e37255d
fujimoto <dai.fuji.110728@gmail.com>
2022-04-09 10:34:35 +0900

    test

--- serverspec/spec/app/app_spec.rb
+++ serverspec/spec/app/app_spec.rb
@@ -26,3 +26,19 @@ end
 describe port(80) do
   it { should be_listening }
 end
+
+
+
+describe command('date') do
+  its(:stdout) { should match /JST/ }
+end
+
+describe user('ec2-user') do
+  it { should exist }
+end
+
+describe file('/var/www/rails_app') do
+  it { should be_directory }
+  it { should be_owned_by 'ec2-user' }
+  it { should be_grouped_into 'ec2-user' }
+end
--- serverspec/spec/db/sample_spec.rb
+++ serverspec/spec/db/sample_spec.rb
@@ -1,27 +1,9 @@
 require 'spec_helper'
 
-describe package('httpd'), :if => os[:family] == 'redhat' do
+describe package('nginx'), :if => os[:family] == 'amazon' do
   it { should be_installed }
 end
 
-describe package('nginx'), :if => os[:family] == 'amazon' do
- it { should be_installed }
-end
-
-describe service('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
-end
 
 describe port(80) do
   it { should be_listening }
